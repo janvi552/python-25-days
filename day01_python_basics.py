7f0892cc5840de83e9e22cc0e4da2f9aad4e7cc5
janvi552 <janvichodvadiya999@gmail.com>
2026-01-21 08:19:49 -0500

    Day 1: Python basics practice

---
+++ day01_python_basics.py
@@ -0,0 +1,62 @@
+#firstprogram
+print("hello world")
+
+#variables
+name="janvi"
+age=18
+marks=89.65
+a=None
+print("my name is ",name)
+print("my age is ",age)
+print(type(marks))
+print(type(age))
+print(type(a))
+
+# print("hello world")
+# print("hello!!")
+# print("hello python")
+
+#arithmatic operators
+a=8
+b=2
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+#relationoperators
+print(a==b)
+print(a<=b)
+print(a>b)
+print(a!=b)
+
+#assignment operators
+num=10
+num+=5
+print("num ",num)
+
+#logicaloperators
+print(not True)
+a=6
+b=9
+print(not(a>=b))
+vol1=True
+vol2=False
+print("and operator ",vol1 and vol2)
+print("or operator ",vol1 or vol2)
+
+#typeconversion
+a,b="2",7
+c=int(a)
+d=float(b)
+sum=b+c
+print(sum)
+print(type(c))
+print(type(d))
+
+#input
+name=input("enter your name ")
+age=input("enter your age ")
+print("welcome",name)
+print("your age is",age)
