3d0d41065b30fca743afd8b6f22cefa01543bb9b
janvi552 <janvichodvadiya999@gmail.com>
2026-01-24 10:14:21 -0500

    Day 1: Practice questions added

---
+++ Day01_Basics/practice_questions.py
@@ -0,0 +1,26 @@
+#two integer's sum
+First=int(input("enter first number"))
+Sec=int(input("enter second number"))
+print("sum= ",First+Sec)
+
+#square's area
+Side=float(input("enter side's length "))
+print("square's area",Side*Side)
+
+#Average of two floating numbers
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+print("two number's average ",(a+b)/2)
+
+#Check a >= b
+a=int(input("enter first:"))
+b=int(input("enter second:"))
+print(a>=b)
+
+a,b=7,"2"
+c=int(b)
+sum=a+c
+print(sum)
+v=float(a)
+print(type(v))
+print(type(a))
